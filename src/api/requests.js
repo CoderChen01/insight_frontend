00ee14a6520e79625d3fe22ba9576ab9d600c4f4
ChenJunjie <1732251769@qq.com>
2021-04-08 13:54:24 +0800

    Optimize the experience

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,6 +2,6 @@ import Axios from 'axios/index'
 
 
 export const requests = Axios.create({
-  baseURL: 'http://192.168.68.134:8080/api/v1/',
+  baseURL: 'http://127.0.0.1:8999/api/v1/',
   timeout: 3 * 60 * 1000
 })
